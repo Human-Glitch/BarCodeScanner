045370acb8cf1a7ec33a4ab73cc7eb07354419b1
Kody Buss <kody.buss@gmail.com>
2024-02-18 21:48:58 -0600

    Setup project

---
+++ BarCodeScanner/BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Kody Buss on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
